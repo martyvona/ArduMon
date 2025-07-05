19a929a3d6d76aeee5f7bab734b42b2e9645174c
Marsette Vona <martyvona@gmail.com>
2025-07-05 15:42:15 -0400

    beef up test code

--- test/host/test_CmdSlave.cpp
+++ test/host/test_CmdSlave.cpp
@@ -1,37 +1,47 @@
 //g++ -o test_CmdSlave test_CmdSlave.cpp --std=c++11
 
 #include <iostream>
-#include <sys/time.h>
-#include <stdio.h>
 
 #include "CmdSlave.h"
 
-extern "C" {
+#define MAX_CMDS 64
+#define RECV_BUF_SZ 256
+#define SEND_BUF_SZ 256
 
-  uint32_t millis() {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    uint64_t now_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-    static uint64_t start_ms = now_ms;
-    return now_ms - start_ms;
-  }
-
-  void delayMicroseconds(uint16_t us) {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    uint64_t start_us = tv.tv_sec * 1000000 + tv.tv_usec, now_us;
-    do {
-      gettimeofday(&tv,NULL);
-      now_us = tv.tv_sec * 1000000 + tv.tv_usec;
-    } while (now_us - start_us < us);
-  }
-}
+CmdSlave<MAX_CMDS,RECV_BUF_SZ,SEND_BUF_SZ> cmd(0, false);
 
 int main(int argc, const char **argv) {
-  std::cout << millis() << "\n";
-  for (int i = 0; i < 20; i++) delayMicroseconds(100);
-  std::cout << millis() << "\n";
-  char buf[100];
-  std::cout << dtostrf(1.23456789e30, 0, 4, buf) << "\n";
-  std::cout << dtostre(1.23456789e30, buf, 4, 0) << "\n";
+
+  cmd.update();
+
+  cmd.recv();
+  char c; cmd.recv(&c);
+
+  const char *s; cmd.recv(&s);
+
+  uint8_t   u8; cmd.recv(&u8);
+  int8_t    s8; cmd.recv(&s8);
+  uint16_t u16; cmd.recv(&u16);
+  int16_t  s16; cmd.recv(&s16);
+  uint32_t u32; cmd.recv(&u32);
+  int32_t  s32; cmd.recv(&s32);
+  uint64_t u64; cmd.recv(&u64);
+  int64_t  s64; cmd.recv(&s64);
+  float      f; cmd.recv(&f);
+  double     d; cmd.recv(&d);
+
+  cmd.send(c); cmd.send_raw(c);
+  cmd.send(s); cmd.send_raw(s);
+
+  cmd.send(u8);
+  cmd.send(s8);
+  cmd.send(u16);
+  cmd.send(s16);
+  cmd.send(u32);
+  cmd.send(s32);
+  cmd.send(u64);
+  cmd.send(s64);
+
+  cmd.send(f);
+  cmd.send(d);
 }

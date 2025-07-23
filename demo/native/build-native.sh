d0dc023cdab9290e86dcade36aa62a2e8fa4199b
Marsette Vona <martyvona@gmail.com>
2025-07-23 14:20:44 -0400

    bugfix compiler warning on os x

--- demo/native/build-native.sh
+++ demo/native/build-native.sh
@@ -4,5 +4,7 @@ src=`ls *.cpp`
 # -Wstringop-overflow=0 suppresses a spurious warning on some versions of g++ with -O3 
 # https://stackoverflow.com/a/75191691
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106757
-g++ -O3 -I../../src -o ${src%.*}_native $src --std=c++11 -Wstringop-overflow=0
+OPTS=
+if ! g++ --version | grep clang > /dev/null; then OPTS=-Wstringop-overflow=0; fi
+g++ -O3 -I../../src -o ${src%.*}_native $src --std=c++11 $OPTS
 

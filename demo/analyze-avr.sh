709eaf313af7574ab4cea65157ccc9158e7db0be
Marsette Vona <martyvona@gmail.com>
2025-08-09 14:12:46 -0400

    add analyze-avr.sh

---
+++ demo/analyze-avr.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# https://arduino.stackexchange.com/questions/31190/detailed-analyse-of-memory-usage
+
+if [ $# -lt 1 ]; then
+  echo "USAGE: analyze-avr.sh BOARD"
+  echo "EXAMPLE: analyze-avr.sh uno"
+  echo "EXAMPLE: analyze-avr.sh nano"
+  exit 1
+fi
+
+adir=`arduino-cli config get directories.data`
+for d in `ls -d $adir/packages/arduino/tools/avr-gcc/*/bin/`; do export PATH="$d:$PATH"; done
+
+elf=`ls ./build/arduino.avr.${1}/*.elf`
+asm=${elf%.elf}.asm
+inf=${elf%.elf}.inf
+
+echo "creating disassembly $asm from $elf"
+avr-objdump -Sz -I./ -I../ $elf > $asm
+
+echo "creating info $inf from $elf"
+avr-readelf -a -W $elf > $inf
+
+
--- demo/build-avr.sh
+++ demo/build-avr.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-  echo "USAGE: build-avr.sh BOARD"
+  echo "USAGE: build-avr.sh BOARD [-d]"
   echo "EXAMPLE: build-avr.sh uno"
   echo "EXAMPLE: build-avr.sh nano"
   exit 1
 fi
 
-arduino-cli compile --fqbn arduino:avr:$1 --libraries ../ -e .
+dbg=
+if [[ $# -gt 1 && $2 == "-d" ]]; then dbg=--optimize-for-debug; fi
+
+arduino-cli compile --fqbn arduino:avr:$1 --libraries ../ -e $dbg .
 

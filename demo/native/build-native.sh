4532c5254681a6d25a14daed2b14f1287d7edf77
Marsette Vona <martyvona@gmail.com>
2025-08-29 13:18:07 -0400

    bugfix error handling in build_native.sh

--- demo/native/build-native.sh
+++ demo/native/build-native.sh
@@ -11,8 +11,8 @@ if ! g++ --version | grep clang > /dev/null; then OPTS=-Wstringop-overflow=0; fi
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "building native text/binary server..."
-g++ -O3 -I${script_dir}/../../src -o demo_native demo.cpp --std=c++11 $OPTS
+g++ -O3 -I${script_dir}/../../src -o demo_native demo.cpp --std=c++11 $OPTS || exit $?
 
 echo "building native binary client..."
-g++ -O3 -I${script_dir}/../../src -o demo_client_native -DBINARY_CLIENT demo.cpp --std=c++11 $OPTS
+g++ -O3 -I${script_dir}/../../src -o demo_client_native -DBINARY_CLIENT demo.cpp --std=c++11 $OPTS || exit $?
 
